3f6f3a4ec50d3ea28adcde078ce4a91449c07fac
danipetro21 <idany346@gmail.com>
2023-01-10 16:29:20 +0100

    print to display hotels

--- index.php
+++ index.php
@@ -2,60 +2,88 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOTEL</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOTEL</title>
 </head>
 
 <body>
 
 
+  <?php
+
+  //data 
+  $hotels = [
+
+    [
+      'name' => 'Hotel Belvedere',
+      'description' => 'Hotel Belvedere Descrizione',
+      'parking' => true,
+      'vote' => 4,
+      'distance_to_center' => 10.4
+    ],
+    [
+      'name' => 'Hotel Futuro',
+      'description' => 'Hotel Futuro Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 2
+    ],
+    [
+      'name' => 'Hotel Rivamare',
+      'description' => 'Hotel Rivamare Descrizione',
+      'parking' => false,
+      'vote' => 1,
+      'distance_to_center' => 1
+    ],
+    [
+      'name' => 'Hotel Bellavista',
+      'description' => 'Hotel Bellavista Descrizione',
+      'parking' => false,
+      'vote' => 5,
+      'distance_to_center' => 5.5
+    ],
+    [
+      'name' => 'Hotel Milano',
+      'description' => 'Hotel Milano Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 50
+    ],
+
+  ];
+
+
+  ?>
+
+
+  <h1>LISTA DEGLI HOTEL</h1>
+
+  <div class="container">
     <?php
+  
+    foreach ($hotels as $hotel) {
+      $name = $hotel["name"];
+      $description = $hotel["description"];
+      $vote = $hotel["vote"];
+      $distance_to_center = $hotel["distance_to_center"];
 
-    //data 
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
+      
+      echo $name . ":" . "<br>";
+      echo $description . " | " . $vote . " | " . $distance_to_center . "<br><br><br>";
+    }
 
     ?>
+  </div>
+
+  
+
+
+
 </body>
+
+
+
 
 </html>
